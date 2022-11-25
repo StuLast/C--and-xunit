3c34a100145f06e0df9ceaea710e7ba749e203db
Stu Last <105198429+Stu-Last@users.noreply.github.com>
2022-11-25 00:05:05 +0000

    Inject RoomBookingRequestProcessor

---
+++ RoomBookingApp/RoomBookingApp.API.Tests/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using Xunit;
+global using Shouldly;
+global using Moq;
+global using System.Linq;
+global using Microsoft.Extensions.Logging;
+global using RoomBookingApp.API;
+global using RoomBookingApp.API.Controllers;
